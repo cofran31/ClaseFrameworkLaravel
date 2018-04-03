77ca1c36d64aa5db9fb22d434ee0ac161d063ae0
Carlos Ortube <cool3000bo@gmail.com>
2018-04-02 22:03:20 -0400

    Ejemplo de routes en laravel clase 1

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/hola', function () {
+    return 'hola mundo';
+});
+Route::get('/objetos/caracteristicas', function () {
+    return 'el id del objeto es:'.$_GET['id'];
+});
+Route::get('/objeto/{id}', function ($id) {
+    return "el id del objeto es:{$id}";
+})->where('id','[0-9]+');
+Route::get('/objeto/nuevo', function () {
+    return "crear nuevo objeto";
+});
+//aliasp? puede o no puede existir 
+Route::get('/alias/{nombrep}/{aliasp?}', function ($nombrep,$aliasp=null) {
+    if($aliasp){
+        return "El producto {$nombrep}, tiene el alias de {$aliasp}";
+    }else{
+    return "El producto {$nombrep}, no tiene el alias ";
+    }
+});
